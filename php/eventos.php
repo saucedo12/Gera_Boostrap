d37d6398c75d69aa505ce0b0c9d57eb6c6aa0a25
humberto <hsr_1988@outlook.com>
2017-06-24 14:02:16 -0600

    Agregando archivos
    todo el contenido hasta 24062017

---
+++ php/eventos.php
@@ -0,0 +1,15 @@
+<?php 
+$fecha1="";
+$fecha2="";
+$fecha3="";
+
+
+ ?>
+
+<form action="" method="POST">
+	<label for="lugar">Lugar</label>
+	<input type="text" name="lugar_text" id="lugar" />
+	<label for="dechat">Fecha</label>
+	<input type="date" name="fecha_date" id="fecha" />
+	<input type="submit">
+</form>
